d66aa450e4bacff3fcb958f84e53b5b81400d05d
annie <annie183150@gmail.com>
2021-11-03 15:57:53 +0800

    first

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <List todoList="您的行程安排" />
   </div>
 </template>
+<script lang="ts">
+import { defineComponent, ref } from "vue"; //下面有用到的方法，這裡都要import
+import axios from "axios";
+import List from "@/components/List.vue";
+
+export default defineComponent({
+  name: "About",
+  components: {
+    List,
+  },
+  setup() {
+    const data = ref({});
+    axios
+      .get(
+        "https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON"
+      )
+      .then((res: any) => {
+        console.log(res.data);
+        data.value = res.data;
+      });
+    return {
+      data,
+    };
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img src="" alt="" />
+    <ul>
+      <li></li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent, ref } from "vue"; //下面有用到的方法，這裡都要import
+import axios from "axios";
 
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  setup() {
+    const data = ref({});
+    axios
+      .get(
+        "https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON"
+      )
+      .then((res: any) => {
+        console.log(res.data);
+        data.value = res.data;
+      });
+    return {
+      data,
+    };
   },
 });
 </script>
